d9fbf969d8ca42f75ed03aade94dc99d33bfccf5
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-07-28 16:15:47 -0300

    Corrigindo requisito 5

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-introducao-javascript/variaveis.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-introducao-javascript/variaveis.js
@@ -1,4 +1,6 @@
 const myName = 'Erickson'
 const birthCity = 'Nova Iguaçu'
-let birthYear = 2030
+let birthYear = 1998
 console.log(`${myName} nasceu em ${birthCity} no ano de ${birthYear}`)
+birthYear = 2030
+console.log(birthYear)
