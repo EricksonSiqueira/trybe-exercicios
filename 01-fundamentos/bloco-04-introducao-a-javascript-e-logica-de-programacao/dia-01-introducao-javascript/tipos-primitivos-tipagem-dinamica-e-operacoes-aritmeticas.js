0b75d505e149405d9675661aa7247e0c0011f0ef
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-07-28 18:08:32 -0300

    Calculando a area

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-introducao-javascript/tipos-primitivos-tipagem-dinamica-e-operacoes-aritmeticas.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-introducao-javascript/tipos-primitivos-tipagem-dinamica-e-operacoes-aritmeticas.js
@@ -1,8 +1,15 @@
-let patientId = 50;
-let isEnrolled = true;
-const patientInfo = {
-  firstName: 'Ana',
-  lastName: 'Santos',
-};
-const patientEmail = 'ana@email.com';
-console.log(typeof(patientId = '50'))
+// let patientId = 50;
+// let isEnrolled = true;
+// const patientInfo = {
+//   firstName: 'Ana',
+//   lastName: 'Santos',
+// };
+// const patientEmail = 'ana@email.com';
+// console.log(typeof(patientId = '50'))
+
+//Exercicio
+let base = 5
+let altura = 8
+
+let area = base * altura
+console.log(area)
