8bf74b8765d79c55db0cc559a730e29253b26c7b
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-07-29 18:28:29 -0300

    Corrigindo exercicio 5, era somente o maior

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-loop-for/exercicios/5-maior-valor-array.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-loop-for/exercicios/5-maior-valor-array.js
@@ -1,17 +1,8 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let maior
-let menor
+let maior = numbers[0]
+
 
 for(let i = 0; i < numbers.length; i += 1){
-
-
-  if(i === 0){ // se estiver no primeiro indice do vetor 
-    maior = numbers[i]
-    menor = numbers[i]
-  }else if(numbers[i] > maior){ // se o valor do vetor for maior que a variavel maior
-    maior = numbers[i]
-  }else if (numbers [i] < menor){ // se o valor do vetor for menor que a variavel menor
-    menor = numbers[i]
-  }
+  if(numbers[i] > maior) maior = numbers[i]  // se o valor do vetor for maior que a variavel maior
 }
-console.log("Maior valor: ", maior, "\nMenor valor: " , menor)
+console.log("Maior valor:", maior)
