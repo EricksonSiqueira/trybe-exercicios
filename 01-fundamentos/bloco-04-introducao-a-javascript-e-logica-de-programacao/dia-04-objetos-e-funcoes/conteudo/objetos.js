85a8404a099b962969584f8b6950ac8f6d3215ec
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-08-02 11:30:28 -0300

    Requisito 4, imprimir array do objeto

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-objetos-e-funcoes/conteudo/objetos.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-objetos-e-funcoes/conteudo/objetos.js
@@ -9,3 +9,4 @@ let player = {
   bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018]
 }
 console.log(`A jogadora ${player.name} ${player.lastName} tem ${player.age} anos de idade.`)
+console.log(`A jogadora ${player.name} ${player.lastName} foi eleita a melhor do mundo por 6 vezes em: ${player.bestInTheWorld}`)
