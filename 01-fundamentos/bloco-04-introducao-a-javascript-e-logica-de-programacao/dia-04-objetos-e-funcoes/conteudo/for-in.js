cbb36dea076ee5225e80915610b86e69f8e5a751
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-08-02 15:17:45 -0300

    Requisito 2 printar chave e valores

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-objetos-e-funcoes/conteudo/for-in.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-objetos-e-funcoes/conteudo/for-in.js
@@ -7,3 +7,15 @@ let names = {
 for(let index in names){
   console.log(`Olá ${names[index]}!`)
 }
+
+console.log('------------------------')
+
+let car = {
+  model: 'A3 Sedan',
+  manufacturer: 'Audi',
+  year: 2020
+};
+
+for(let index in car){
+  console.log(`${index}: ${car[index]}`)
+}
