8bed50b7d060cf468a8581026ea3171d5af38f2c
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-08-09 15:03:58 -0300

    Selecionando e mudando cor de fundo do header

--- 01-fundamentos/bloco-05-javascript--dom-eventos-e-web-storage/dia-01-dom-e-seletores/conteudo/administrador-do-tempo-da-trybe/script.js
+++ 01-fundamentos/bloco-05-javascript--dom-eventos-e-web-storage/dia-01-dom-e-seletores/conteudo/administrador-do-tempo-da-trybe/script.js
@@ -3,6 +3,7 @@ const emergencySection = document.querySelector('.emergency-tasks')
 const noEmergencySection = document.querySelector('.no-emergency-tasks')
 const noEmergencyH3 = document.querySelectorAll('.no-emergency-tasks h3')
 const footer = document.getElementById('footer-container')
+const header = document.getElementById('header-container')
 
 emergencySection.style.background = '#ff9f84'
 noEmergencySection.style.background = '#F9DB5E'
@@ -11,3 +12,4 @@ for(let i = 0;i < emergencyH3.length; i += 1){
   noEmergencyH3[i].style.background = '#000000'
 }
 footer.style.background = '#003533'
+header.style.background = '#00B069'
