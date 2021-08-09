ad6143790d8f47c34954afa3a16a771ae559f54d
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-08-09 11:43:19 -0300

    Modificando a cor pela section e colocando paddingTop de 10px nas divs da section emergency-tasks

--- 01-fundamentos/bloco-05-javascript--dom-eventos-e-web-storage/dia-01-dom-e-seletores/conteudo/administrador-do-tempo-da-trybe/script.js
+++ 01-fundamentos/bloco-05-javascript--dom-eventos-e-web-storage/dia-01-dom-e-seletores/conteudo/administrador-do-tempo-da-trybe/script.js
@@ -1,7 +1,11 @@
 const emergencyDivs = document.querySelectorAll('.emergency-tasks div')
 const emergencyH3 = document.querySelectorAll('.emergency-tasks div h3')
+const emergencySection = document.querySelector('.emergency-tasks')
 
+emergencySection.style.background = '#ff9f84'
 for(let i = 0;i < emergencyDivs.length; i += 1){
-  emergencyDivs[i].style.background = '#ff9f84'
+  emergencyDivs[i].style.paddingTop = '10px'
   emergencyH3[i].style.background = '#a51ff3'
+  emergencyH3[i].style.marginTop = '0px';
 }
+emergencyH3[0].style.padding = '10px'
