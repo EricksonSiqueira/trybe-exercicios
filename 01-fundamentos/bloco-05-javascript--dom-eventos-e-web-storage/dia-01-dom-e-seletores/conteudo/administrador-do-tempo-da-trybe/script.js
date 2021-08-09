783a7154dede7570c7a989966db7efeeb40f4388
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-08-09 15:02:14 -0300

    Selecionando e mudando cor de fundo do footer

--- 01-fundamentos/bloco-05-javascript--dom-eventos-e-web-storage/dia-01-dom-e-seletores/conteudo/administrador-do-tempo-da-trybe/script.js
+++ 01-fundamentos/bloco-05-javascript--dom-eventos-e-web-storage/dia-01-dom-e-seletores/conteudo/administrador-do-tempo-da-trybe/script.js
@@ -2,6 +2,7 @@ const emergencyH3 = document.querySelectorAll('.emergency-tasks div h3')
 const emergencySection = document.querySelector('.emergency-tasks')
 const noEmergencySection = document.querySelector('.no-emergency-tasks')
 const noEmergencyH3 = document.querySelectorAll('.no-emergency-tasks h3')
+const footer = document.getElementById('footer-container')
 
 emergencySection.style.background = '#ff9f84'
 noEmergencySection.style.background = '#F9DB5E'
@@ -9,3 +10,4 @@ for(let i = 0;i < emergencyH3.length; i += 1){
   emergencyH3[i].style.background = '#a51ff3'
   noEmergencyH3[i].style.background = '#000000'
 }
+footer.style.background = '#003533'
