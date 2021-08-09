7f701b4dc7d8c9ea6b6236a0d7b6d2f65b253f15
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-08-09 11:45:01 -0300

    Removendo algumas alteraçoes inuteis

--- 01-fundamentos/bloco-05-javascript--dom-eventos-e-web-storage/dia-01-dom-e-seletores/conteudo/administrador-do-tempo-da-trybe/script.js
+++ 01-fundamentos/bloco-05-javascript--dom-eventos-e-web-storage/dia-01-dom-e-seletores/conteudo/administrador-do-tempo-da-trybe/script.js
@@ -4,7 +4,5 @@ const emergencySection = document.querySelector('.emergency-tasks')
 
 emergencySection.style.background = '#ff9f84'
 for(let i = 0;i < emergencyDivs.length; i += 1){
-  emergencyDivs[i].style.paddingTop = '10px'
   emergencyH3[i].style.background = '#a51ff3'
-  emergencyH3[i].style.marginTop = '0px';
 }
