37c2bc664c7ad1473fab84241b6ca042aa968fe0
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-08-09 14:53:04 -0300

    Selecionando e mudando cor da seçao no-emergency-tasks

--- 01-fundamentos/bloco-05-javascript--dom-eventos-e-web-storage/dia-01-dom-e-seletores/conteudo/administrador-do-tempo-da-trybe/script.js
+++ 01-fundamentos/bloco-05-javascript--dom-eventos-e-web-storage/dia-01-dom-e-seletores/conteudo/administrador-do-tempo-da-trybe/script.js
@@ -1,7 +1,9 @@
 const emergencyH3 = document.querySelectorAll('.emergency-tasks div h3')
 const emergencySection = document.querySelector('.emergency-tasks')
+const noEmergencySection = document.querySelector('.no-emergency-tasks')
 
 emergencySection.style.background = '#ff9f84'
+noEmergencySection.style.background = '#F9DB5E'
 for(let i = 0;i < emergencyH3.length; i += 1){
   emergencyH3[i].style.background = '#a51ff3'
 }
