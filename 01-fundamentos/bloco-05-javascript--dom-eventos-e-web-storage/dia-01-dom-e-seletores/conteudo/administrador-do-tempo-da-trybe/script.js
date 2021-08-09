40454a4742048856191112ea27323f155fa86260
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-08-09 14:55:10 -0300

    Selecionando e mudando cor dos h3 dentro da no-emergency-tasks

--- 01-fundamentos/bloco-05-javascript--dom-eventos-e-web-storage/dia-01-dom-e-seletores/conteudo/administrador-do-tempo-da-trybe/script.js
+++ 01-fundamentos/bloco-05-javascript--dom-eventos-e-web-storage/dia-01-dom-e-seletores/conteudo/administrador-do-tempo-da-trybe/script.js
@@ -1,9 +1,11 @@
 const emergencyH3 = document.querySelectorAll('.emergency-tasks div h3')
 const emergencySection = document.querySelector('.emergency-tasks')
 const noEmergencySection = document.querySelector('.no-emergency-tasks')
+const noEmergencyH3 = document.querySelectorAll('.no-emergency-tasks h3')
 
 emergencySection.style.background = '#ff9f84'
 noEmergencySection.style.background = '#F9DB5E'
 for(let i = 0;i < emergencyH3.length; i += 1){
   emergencyH3[i].style.background = '#a51ff3'
+  noEmergencyH3[i].style.background = '#000000'
 }
