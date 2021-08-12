cee9bff0d21e836350693528302a91bd4c43c2c6
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-08-12 20:52:40 -0300

    Adiciona funçao que muda e armazena cor do texto

--- 01-fundamentos/bloco-05-javascript--dom-eventos-e-web-storage/dia-04-web-storage/exercicios/script.js
+++ 01-fundamentos/bloco-05-javascript--dom-eventos-e-web-storage/dia-04-web-storage/exercicios/script.js
@@ -1,4 +1,5 @@
-let colorsOptions = document.querySelector('.colors')
+let colorsOptions = document.querySelector('.bg-colors')
+let textColorOptions = document.querySelector('.text-colors')
 let backgroundColor = localStorage.getItem('bgcolor')
 let body = document.body
 
@@ -9,6 +10,16 @@ colorsOptions.addEventListener('click', function(e) {
     if(element.classList.contains(color)){
       body.style.backgroundColor = color
       localStorage.setItem('bgcolor', color)
+    }
+  }
+})
+textColorOptions.addEventListener('click', function(e){
+  let element = e.target
+  let colors = ['pink', 'purple', 'grey']
+  for(color of colors){
+    if(element.classList.contains(color)){
+      body.style.color = color
+      localStorage.setItem('textColor', color)
     }
   }
 })
