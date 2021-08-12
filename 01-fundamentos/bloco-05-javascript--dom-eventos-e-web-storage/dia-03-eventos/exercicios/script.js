d7d9369eadbd4b6a4ad1d425f10adc018a2a936b
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-08-11 21:19:54 -0300

    Criando dias e adicionando suas respectivas classes

---
+++ 01-fundamentos/bloco-05-javascript--dom-eventos-e-web-storage/dia-03-eventos/exercicios/script.js
@@ -0,0 +1,57 @@
+function createDaysOfTheWeek() {
+  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+  const weekDaysList = document.querySelector('.week-days');
+
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = days;
+
+    weekDaysList.appendChild(dayListItem);
+  };
+};
+
+createDaysOfTheWeek();
+
+// Escreva seu código abaixo.
+const daysList = document.querySelector('#days')
+
+function checkFriday(dayText){
+  let fridays = ['4', '11', '18', '25']
+  let boolean = false
+
+  for(friday of fridays){
+    if(friday === dayText){
+      boolean = true
+    }
+  }
+  return boolean
+}
+function checkHoliday(dayText){
+  let holidays = ['24', '25', '31']
+  let boolean = false
+  for(let day of holidays){
+    if(day === dayText){
+      boolean = true
+    }
+  }
+  return boolean
+}
+function creatDaysLi(numberOfDays){
+  for(let i = 0; i < numberOfDays; i += 1){
+    let day = document.createElement('li')
+    day.innerText = i+1
+    day.classList.add('day')
+    
+    if(checkHoliday(day.innerText)){
+      day.classList.add('holiday')
+    }
+    if(checkFriday(day.innerText)){
+      day.classList.add('friday')
+    }
+    daysList.appendChild(day)
+  }
+
+}
+
+creatDaysLi(31)
