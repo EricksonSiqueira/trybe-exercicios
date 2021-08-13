326b183676510a1097288ab219952577e9f94e5a
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-08-12 21:22:01 -0300

    Adiciona opçao aumenta fonte e espaçamento entre as linhas

--- 01-fundamentos/bloco-05-javascript--dom-eventos-e-web-storage/dia-04-web-storage/exercicios/script.js
+++ 01-fundamentos/bloco-05-javascript--dom-eventos-e-web-storage/dia-04-web-storage/exercicios/script.js
@@ -1,6 +1,7 @@
 let colorsOptions = document.querySelector('.bg-colors')
 let textColorOptions = document.querySelector('.text-colors')
-let backgroundColor = localStorage.getItem('bgcolor')
+let fontSizeOptions = document.querySelector('.font-size')
+let lineSpacingOptions = document.querySelector('.line-spacing ')
 let body = document.body
 
 colorsOptions.addEventListener('click', function(e) {
@@ -23,8 +24,38 @@ textColorOptions.addEventListener('click', function(e){
     }
   }
 })
+fontSizeOptions.addEventListener('click',function (e){
+  let element = e.target
+  let fontSizes = [15, 20, 30]
+  let fontClasses = ['small', 'medium', 'big']
+
+  for(let i = 0; i < fontSizes.length; i += 1){
+    if(element.classList.contains(fontClasses[i])){
+      body.style.fontSize = fontSizes[i] + 'px'
+      localStorage.setItem('fontSize', fontSizes[i])
+    }
+  }
+
+})
+lineSpacingOptions.addEventListener('click', function (e) {
+  let element = e.target
+  let fontSizes = [18, 25]
+  let fontClasses = ['medium', 'big']
+
+  for (let i = 0; i < fontSizes.length; i += 1) {
+    if (element.classList.contains(fontClasses[i])) {
+      body.style.lineHeight = fontSizes[i] + 'px'
+      localStorage.setItem('fontSize', fontSizes[i])
+    }
+  }
+
+})
 
 function pagePreSets(){
+  let backgroundColor = localStorage.getItem('bgcolor')
+  let textColor = localStorage.getItem('textColor')
+
   body.style.backgroundColor = backgroundColor
+  body.style.color = textColor
 }
 pagePreSets()
