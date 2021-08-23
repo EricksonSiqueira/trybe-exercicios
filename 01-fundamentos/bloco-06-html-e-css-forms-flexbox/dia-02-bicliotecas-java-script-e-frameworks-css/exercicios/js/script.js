f537dcf55201c598e0c0a4b21e6cd0afdd27c099
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-08-23 11:52:59 -0300

    Removendo a funçao que verificava data

--- 01-fundamentos/bloco-06-html-e-css-forms-flexbox/dia-02-bicliotecas-java-script-e-frameworks-css/exercicios/js/script.js
+++ 01-fundamentos/bloco-06-html-e-css-forms-flexbox/dia-02-bicliotecas-java-script-e-frameworks-css/exercicios/js/script.js
@@ -15,42 +15,7 @@ function creatStateOptions() {
     states.appendChild(option)
   }
 }
-const inputDate = document.getElementById('start-date')
-function verifyDay(item, itemText) {
-  let boolean = false
-  if (item !== NaN && itemText === 2 && item > 0 && item <= 31) {
-   boolean = true
-  }
-  return boolean
-}
-function verifyMonth(item, itemText) {
-  let boolean = false
-  if (item !== NaN && itemText === 2 && item > 0 && item <= 12) {
-    boolean = true
-  }
-  return boolean
-}
 
-function verifyYear(year, yearText) {
-  let boolean = false
-  if (year !== NaN && yearText === 4 && year > 0) {
-    boolean = true
-  }
-  return boolean
-}
-function verifyDate(){
-  const valueSplited = inputDate.value.split('/')
-  const dayTxtSize = valueSplited[0].length
-  const monthTxtSize = valueSplited[1].length
-  const yearTxtSize = valueSplited[2].length
-  const day = parseInt(valueSplited[0])
-  const month = parseInt(valueSplited[1])
-  const year = parseInt(valueSplited[2])
-
-  if (verifyDay(day, dayTxtSize) ==! true || verifyMonth(month, monthTxtSize) ==! true || verifyYear(year, yearTxtSize) ==! true){
-   window.alert('Tem algo errado com a data. Verifique se esta no estilo dd/mm/aaaa (dia, mês e ano)') 
-  }
-}
 function verifyLength (input, maxLength) {
   const inputValue = input.value
   let boolean = true
@@ -106,7 +71,5 @@ submitButton.addEventListener('click', (event) => {
     window.alert('CPF válido')
   }
 })
-
-buttonTeste.addEventListener('click', verifyLength)
 
 creatStateOptions()
