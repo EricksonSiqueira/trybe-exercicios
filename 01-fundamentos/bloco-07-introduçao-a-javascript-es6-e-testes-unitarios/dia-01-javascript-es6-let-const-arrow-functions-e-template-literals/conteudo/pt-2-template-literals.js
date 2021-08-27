860db0f285c6c0d4fee287aa8988ca7bb8ee7a51
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-08-27 14:48:43 -0300

    Parte 2, FINALMENTE ENSINARAM TEMPLATE LITERALS!

---
+++ 01-fundamentos/bloco-07-introduçao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/conteudo/pt-2-template-literals.js
@@ -0,0 +1,13 @@
+// const myName = "Isabella"
+// console.log('Hello' + ' ' + myName + '!');
+
+const myName = "Isabella"
+console.log(`Welcome ${myName}!`);
+
+console.log(`Primeira linha;
+Segunda linha;
+Terceira linha;`
+)
+
+// Sem o template literals:
+console.log('Primeira linha 2;\n' + 'Segunda linha 2;\n' + 'Terceira linha 2;\n')
