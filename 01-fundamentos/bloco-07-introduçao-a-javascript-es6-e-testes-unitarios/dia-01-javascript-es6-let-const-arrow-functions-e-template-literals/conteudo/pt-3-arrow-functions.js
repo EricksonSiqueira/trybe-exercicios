4a7a50009c847196a0dc42d239666a79befaf450
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-08-27 15:14:33 -0300

    Parte 3, aprendendo a usar arrow functions

---
+++ 01-fundamentos/bloco-07-introduçao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/conteudo/pt-3-arrow-functions.js
@@ -0,0 +1,16 @@
+// const printName = function () {
+//   const myName = "Lucas";
+//   return myName;
+// }
+const printName = () => "Lucas"
+console.log(printName());
+
+const multipliByTwo = number => number * 2
+console.log(multipliByTwo(2))
+
+const multiplication = (number, multiplicator) => number * multiplicator
+console.log(multiplication(4, 5))
+
+// const objetoPessoa = (nome, idade) => ({nome, idade})
+
+// console.log(objetoPessoa('Erickson', '23'))
