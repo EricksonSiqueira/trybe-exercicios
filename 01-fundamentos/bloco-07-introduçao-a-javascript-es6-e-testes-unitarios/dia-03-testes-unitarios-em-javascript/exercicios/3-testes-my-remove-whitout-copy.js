e6939bc654d06203ddbc0cb9b2be5a06ddf07af9
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-08-31 18:43:30 -0300

    Teste 3, testes da myRemoveWithoutCopy

---
+++ 01-fundamentos/bloco-07-introduçao-a-javascript-es6-e-testes-unitarios/dia-03-testes-unitarios-em-javascript/exercicios/3-testes-my-remove-whitout-copy.js
@@ -0,0 +1,24 @@
+const assert = require('assert');
+
+function myRemoveWithoutCopy(arr, item) {
+  for (let index = 0, len = arr.length; index < len; index += 1) {
+    if (arr[index] === item) {
+      arr.splice(index, 1);
+      index -= 1;
+      len -= 1;
+    }
+  }
+
+  return arr;
+}
+
+// implemente seus testes aqui
+assert.strictEqual(typeof myRemoveWithoutCopy, 'function');
+assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 4]);
+assert.notDeepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+
+const listArray = [1 ,2 ,4 ,5 ,7];
+console.log(myRemoveWithoutCopy(listArray, 5));
+console.log(listArray);
+
+assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 5), [1, 2, 3, 4]);
