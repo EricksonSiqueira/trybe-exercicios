269da021ad9d9910e5af4132d3ba8d397a25a633
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-09-03 18:48:05 -0300

    Retornando livro com nome de 26 caracteres

--- 01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-higher-order-functions-foreach-find-some-every-sorte/exercicios/exercicio.js
+++ 01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-higher-order-functions-foreach-find-some-every-sorte/exercicios/exercicio.js
@@ -87,3 +87,22 @@ function smallerName() {
 }
 
 assert.strictEqual(smallerName(), 'Duna');
+
+//exercicio 3
+const expectedResult = {
+  author: {
+    birthYear: 1948,
+    name: 'George R. R. Martin',
+  },
+  genre: 'Fantasia',
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  releaseYear: 1991,
+};
+
+function getNamedBook() {
+  // escreva seu código aqui
+  return books.find( book => book.name.length === 26);
+}
+
+assert.deepStrictEqual(getNamedBook(), expectedResult);
