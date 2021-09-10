a8e54286d7f6f7be6ee00ddfb03d9dd68662f4c0
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-09-10 15:56:08 -0300

    Extraindo valores de um array com array destructuring

---
+++ 01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-spread-operator-parametro-rest-destructuring-e-mais/conteudo/array-destructuring.js
@@ -0,0 +1,40 @@
+// const arrayCountries = ['Brazil', 'Japan', 'China', 'Canada'];
+
+// const firstCountry = arrayCountries[0];
+// const secondCountry = arrayCountries[1];
+// const thirdCountry = arrayCountries[2];
+// const fourthCountry = arrayCountries[3];
+
+// console.log(firstCountry); // Brazil
+// console.log(secondCountry); // Japan
+// console.log(thirdCountry); // China
+// console.log(fourthCountry); // Canada
+
+// const arrayCountries = ['Brazil', 'Japan', 'China', 'Canada'];
+// const [firstCountry, secondCountry, thirdCountry, fourthCountry] = arrayCountries;
+
+// console.log(firstCountry); // Brazil
+// console.log(secondCountry); // Japan
+// console.log(thirdCountry); // China
+// console.log(fourthCountry); // Canada
+
+// const saudacoes = ['Olá', (saudacao) => console.log(saudacao)];
+
+// saudacoes[1](saudacoes[0]); // Olá
+
+// // Produza o mesmo resultado acima, porém utilizando array destructuring
+
+// const [greeting, printGreeting] = saudacoes;
+// printGreeting(greeting);
+
+// let comida = 'gato';
+// let animal = 'água';
+// let bebida = 'arroz';
+
+// console.log(comida, animal, bebida); // arroz gato água
+
+// // Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
+// const [animal2, bebida2, comida2] = [comida, animal, bebida];
+
+// console.log(comida2, animal2, bebida2);
+
