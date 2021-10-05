f137e80d1fa50ad6698e83776b7317f5bc934fc3
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-10-05 20:49:52 -0300

    Implementando iteraçao do items criando Order para cada item com key=id

--- 02-front-end/bloco-10-B-introducao-a-react/dia-02-componentes-react/conteudo/fixation-exercises-10-b-2/src/App.js
+++ 02-front-end/bloco-10-B-introducao-a-react/dia-02-componentes-react/conteudo/fixation-exercises-10-b-2/src/App.js
@@ -5,31 +5,39 @@ import Order from './Order';
 
 class App extends React.Component {
   render() {
-    const headphone = {
-      id: 102,
-      user: "cena@gmail.com",
-      product: "Razer Headphone",
-      price: {
-        value: 99.99,
-        currency: "dollars"
+    const items =[
+      {
+        id: 102,
+        user: "cena@gmail.com",
+        product: "Razer Headphone",
+        price: {
+          value: 99.99,
+          currency: "dollars"
+        }
+      },
+      {
+        id: 77,
+        user: "cena@gmail.com",
+        product: "Monster 500mL",
+        price: {
+          value: 9.99,
+          currency: "dollars"
+        }
       }
-    };
-
-    const energyDrink = {
-      id: 77,
-      user: "cena@gmail.com",
-      product: "Monster 500mL",
-      price: {
-        value: 9.99,
-        currency: "dollars"
-      }
-    };
+  
+    ];
+    
 
     return (
+      // <div className="App">
+      //   <h1> Orders recently created </h1>
+      //   <Order order={headphone}/>
+      //   <Order order={energyDrink} />
+      // </div>
+      //Exercicio 2 forma dinamica
       <div className="App">
         <h1> Orders recently created </h1>
-        <Order order={headphone}/>
-        <Order order={energyDrink} />
+        { items.map((item) => <Order key={item.id} order={item}/> ) }
       </div>
     );
   }
