6c57ee104843b5bcd4aaee62c505bad20ecc1c1c
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-10-07 16:26:12 -0300

    Aprendendo a atualizar o estado utilizando: this.state e this.setState

--- 02-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estados-e-eventos/conteudo/fancy-buttons/src/App.js
+++ 02-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estados-e-eventos/conteudo/fancy-buttons/src/App.js
@@ -5,15 +5,20 @@ import './App.css';
 class App extends React.Component {
   constructor() {
     super();
+    this.state = {
+      numeroDeCliques: 0,
+    }
     this.handleClick = this.handleClick.bind(this);
   }
   handleClick() {
-    console.log(this);
-    console.log('Clicou no botão!')
+    this.setState((previousState, _props) => ({
+      numeroDeCliques: previousState.numeroDeCliques + 1,
+    }))
+    console.log(this.state.numeroDeCliques);
   }
   render() {
     return (
-      <button onClick={this.handleClick}>Meu botão</button>
+      <button onClick={this.handleClick}>{this.state.numeroDeCliques}</button>
     );
 
   }
