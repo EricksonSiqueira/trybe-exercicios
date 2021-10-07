3344c8fa1096f100969d74fe89db17ac8409c5b6
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-10-07 15:53:34 -0300

    Utilizando constructor, super e bind para dar acesso ao this numa funçao declarada dentro do componente

---
+++ 02-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estados-e-eventos/conteudo/fancy-buttons/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    console.log(this);
+    console.log('Clicou no botão!')
+  }
+  render() {
+    return (
+      <button onClick={this.handleClick}>Meu botão</button>
+    );
+
+  }
+}
+
+export default App;
