095f2fc8a54df89916262cd1a3e2c320c8114b1c
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-10-08 16:10:48 -0300

    Colocando input de file e filedset

--- 02-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-02-formularios-no-react/conteudo/form-conteudo/src/Form.js
+++ 02-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-02-formularios-no-react/conteudo/form-conteudo/src/Form.js
@@ -5,24 +5,28 @@ class Form extends Component {
     super()
 
     this.handleChange = this.handleChange.bind(this);
+    this.fileInput = React.createRef();
 
     this.state = {
       favoritState: '',
       email: '',
       age: 0,
-      willCome:false,
+      willCome: false,
       day: '08/09',
+      file: '',
     }
+
   }
 
-  handleChange( {target} ) {
+  handleChange({ target }) {
     const { name } = target;
     const value = (target.type === 'checkbox') ? target.checked : target.value;
-    this.setState({[name]: value})
+    this.setState({ [name]: value })
   }
-  render(){
-    const {favoritState ,email, age, willCome, day} = this.state;
-    return(
+
+  render() {
+    const { favoritState, email, age, willCome, day } = this.state;
+    return (
       <div>
         <h1>Meu primeiro formulário usando React</h1>
         <form className="form">
@@ -30,24 +34,27 @@ class Form extends Component {
             Diga qual o seu Estado favorito! De React ou do Brasil, você decide! =)
             <textarea name="favoritState" value={favoritState} onChange={this.handleChange} />
           </label>
-          <label>
-            email:
-            <input
-              type="email"
-              name="email"
-              value={email}
-              onChange={this.handleChange}
-            />
-          </label>
-          <label>
-            Sua idade:
-            <input
-              type="number"
-              name="age"
-              value={age}
-              onChange={this.handleChange}
-            />
-          </label>
+          <fieldset>
+            <legend>Informações do usuário</legend>
+            <label>
+              email:
+              <input
+                type="email"
+                name="email"
+                value={email}
+                onChange={this.handleChange}
+              />
+            </label>
+            <label>
+              Sua idade:
+              <input
+                type="number"
+                name="age"
+                value={age}
+                onChange={this.handleChange}
+              />
+            </label>
+          </fieldset>
           <label>
             Vai comparecer?
             <input
@@ -63,6 +70,10 @@ class Form extends Component {
             <option value="09/09">09/09</option>
             <option value="10/09">10/09</option>
           </select>
+          <label>
+            Sua foto:
+            <input type="file" ref={this.fileInput} />
+          </label>
         </form>
       </div>
     )
