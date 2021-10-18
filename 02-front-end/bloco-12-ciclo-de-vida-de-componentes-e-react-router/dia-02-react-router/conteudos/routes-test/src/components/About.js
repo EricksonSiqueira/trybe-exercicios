0e4607f498ff370604681b1b6a42b69bbcf071ce
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-10-18 15:25:54 -0300

    Utilizando Link para redirecionar o usuario para paginas

--- 02-front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-02-react-router/conteudos/routes-test/src/components/About.js
+++ 02-front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-02-react-router/conteudos/routes-test/src/components/About.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 class About extends React.Component {
   render() { 
     return (
       <div>
+        <Link to="/">Get back to home</Link>
         <h2>Talking <em>about what this page is about</em></h2>
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam numquam explicabo officia sit ratione ab beatae cumque? Officia recusandae excepturi, iure qui, nam quos repellat quibusdam soluta sint, non rerum.</p>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae, laudantium! Assumenda dolore quisquam consectetur in debitis accusamus minus at eveniet dignissimos earum excepturi eum exercitationem amet asperiores, natus cupiditate provident.</p>
