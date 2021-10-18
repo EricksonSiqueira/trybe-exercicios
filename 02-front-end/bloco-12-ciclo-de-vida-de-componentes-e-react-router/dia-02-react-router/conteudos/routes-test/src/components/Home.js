d07e31ab91557c565fb282156028bc52273ae1d8
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-10-18 15:13:04 -0300

    Fazendo minha segunda rota

--- 02-front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-02-react-router/conteudos/routes-test/src/components/Home.js
+++ 02-front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-02-react-router/conteudos/routes-test/src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import About from './About';
+import HowToUse from './HowToUse';
 import { Route } from 'react-router-dom';
 
 class Home extends React.Component {
@@ -7,7 +8,8 @@ class Home extends React.Component {
     return (
       <div>
         <h1>This is my home page</h1>
-        <Route path="/about" component={About} />
+        <Route path="/about" component={ About } />
+        <Route path="/howtouse" component={ HowToUse } />
       </div>
     );
   }
