04e4b10e8ea619c954f7df09a436b17c91dce12a
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-10-18 15:02:19 -0300

    Criando arquivos para fazer o exercicio

---
+++ 02-front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-02-react-router/conteudos/routes-test/src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Home extends React.Component {
+  render() { 
+    return (
+      <div>
+        <h1>This is my home page</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor ut cumque maxime nemo quibusdam minima deserunt eligendi commodi corporis id nobis unde nam est odit, consequatur molestias illo adipisci repellendus!</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et suscipit praesentium minima delectus voluptate aspernatur inventore, sequi obcaecati qui ratione cupiditate placeat, veritatis eos accusamus, commodi iure aperiam! Explicabo, nihil.</p>
+      </div>
+    );
+  }
+}
+ 
+export default Home;
