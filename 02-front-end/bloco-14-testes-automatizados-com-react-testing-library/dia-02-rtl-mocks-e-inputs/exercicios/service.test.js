a89f2a4e6afa04f8076c36f432cb0055421849c0
Erickson <ericksonsiqueirasilva@gmail.com>
2021-11-03 20:34:07 -0300

    Reimplementando funçao randomNumber 2 vezes no mesmo teste

--- 02-front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-02-rtl-mocks-e-inputs/exercicios/service.test.js
+++ 02-front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-02-rtl-mocks-e-inputs/exercicios/service.test.js
@@ -15,4 +15,18 @@ describe('Testes do arquivo service.', () => {
     expect(randomNumber).toHaveBeenCalledTimes(1);
     expect(randomNumber(6, 3)).not.toBe(2);
   })
+
+  it('Reimplementando randomNumber 2 vezes', () => {
+    randomNumber = jest.fn().mockImplementation((number1, number2, number3) => (number1 * number2 * number3) );
+    expect(randomNumber(2, 3, 4)).toBe(24);
+    expect(randomNumber).toHaveBeenCalled();
+    expect(randomNumber).toHaveBeenCalledTimes(1);
+
+    randomNumber = jest.fn().mockImplementation((number) => (number * 2) );
+    expect(randomNumber(5)).toBe(10);
+    expect(randomNumber).toHaveBeenCalled();
+    expect(randomNumber).toHaveBeenCalledTimes(1);
+    expect(randomNumber(4)).toBe(8);
+    expect(randomNumber).toHaveBeenCalledTimes(2);
+  })
 })
