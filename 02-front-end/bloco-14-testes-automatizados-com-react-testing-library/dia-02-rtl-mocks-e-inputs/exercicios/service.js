5f400dca116f2cd165b94de58633059cccec5b5e
Erickson <ericksonsiqueirasilva@gmail.com>
2021-11-04 15:22:27 -0300

    Exercicio 5 imcompleto, problema com o global

--- 02-front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-02-rtl-mocks-e-inputs/exercicios/service.js
+++ 02-front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-02-rtl-mocks-e-inputs/exercicios/service.js
@@ -1,3 +1,5 @@
+const fetch = require('node-fetch');
+
 const randomNumber = () => Math.floor(Math.random()*101);
 
 const stringToUpperCase = (string) => string.toUpperCase();
@@ -6,4 +8,18 @@ const firstChar = (string) => string[0];
 
 const concatTwoStrings = (string1, string2) => `${string1}${string2}`
 
-module.exports = { randomNumber, stringToUpperCase, firstChar, concatTwoStrings };
+const fetchDogPicture = async () => {
+  const URL = 'https://dog.ceo/api/breeds/image/random'
+
+  try {
+    const response = await fetch(URL);
+    const dogPicture = await response.json();
+    return (dogPicture);
+  } catch(error) {
+    console.log(`Algo deu errado ${error}`);
+  }
+}
+
+console.log(fetch);
+
+module.exports = { randomNumber, stringToUpperCase, firstChar, concatTwoStrings, fetchDogPicture };
