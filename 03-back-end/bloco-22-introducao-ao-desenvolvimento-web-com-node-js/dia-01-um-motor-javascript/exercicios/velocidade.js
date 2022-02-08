65c015411459e91393a426b296e30bf53f998046
Erickson <ericksonsiqueirasilva@gmail.com>
2022-02-08 18:52:45 -0300

    Criando modulo que calcula velovidade media

---
+++ 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-01-um-motor-javascript/exercicios/velocidade.js
@@ -0,0 +1,16 @@
+const readline = require('readline-sync');
+
+function calcVelMedia (distancia, tempo) {
+  return distancia/tempo;
+};
+
+function getVelMedia () {
+  const distancia = readline.questionInt('Qual a distância percorrida?(m) ');
+  const velocidade = readline.questionInt('Qual a velocidade?(s) ');
+
+  const velMedia = calcVelMedia(distancia, velocidade);
+
+  console.log(`A velocidade média é ${velMedia.toFixed(2)} m/s`);
+}
+
+getVelMedia();
