3903f3b76783e0b58dc1284c59dfe08fed025e03
Erickson <ericksonsiqueirasilva@gmail.com>
2022-02-10 16:37:17 -0300

    Criando testes e corrigindo erro no codigo

--- 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-03-testes-de-back-end-com-node-js/conteudo/tests/calculaSituacao.test.js
+++ 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-03-testes-de-back-end-com-node-js/conteudo/tests/calculaSituacao.test.js
@@ -7,4 +7,20 @@ describe('Quando a média for menor que 7', () => {
 
     expect(resposta).to.be.equals('reprovado');
   });
-})
+});
+
+describe('Quando a média for maior que 7', () => {
+  it('Retorna "aprovado"', () => {
+    const resposta = calculaSituacao(8);
+
+    expect(resposta).to.be.equals('aprovado');
+  })
+});
+
+describe('Quando a édia for igual a 7', () => {
+  it('Retorna "aprovado"', () => {
+    const resposta = calculaSituacao(7);
+
+    expect(resposta).to.be.equals('aprovado');
+  })
+});
