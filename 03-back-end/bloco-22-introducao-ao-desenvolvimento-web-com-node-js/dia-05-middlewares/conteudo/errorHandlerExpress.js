9e4cc8bf15ad2a20207e0a8df622b0aad5f39966
Erickson <ericksonsiqueirasilva@gmail.com>
2022-02-14 17:57:15 -0300

    Criando erros especificos

--- 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-05-middlewares/conteudo/errorHandlerExpress.js
+++ 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-05-middlewares/conteudo/errorHandlerExpress.js
@@ -2,6 +2,8 @@ const express = require('express');
 const rescue = require('express-recue');
 const fs = require('fs/promises');
 
+const errorMiddleware = require('./errorMiddleware');
+
 const app = express();
 
 app.get(
@@ -9,11 +11,18 @@ app.get(
   rescue(async (req, res) => {
     const file = await fs.readFile(`./${req.params.fileName}`);
     res.send(file.toString('utf8'));
-  })
+  }),
+  (err, _req, _res, next) => {
+    if(err.code === 'ENOENT') {
+      const newError = new Error(err.message);
+      newError.code = 'file_not_found';
+      newError.status = 404;
+      return next(newError);
+    }
+
+    return next(err);
+  }
 );
 
-app.use((err, _req, res, _next) => {
-  res.status(500).json({ error: `Erro ${err.message}`});
-});
-
+app.use(errorMiddleware);
 app.listen(3002);
