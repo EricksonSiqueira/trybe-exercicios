f1032d1ca5f8e1e904441a893f139fbfa1ddf920
Erickson <ericksonsiqueirasilva@gmail.com>
2022-03-24 20:35:50 -0300

    feat: type unions

---
+++ 03-back-end/bloco-26-typescript/dia-02-tipagem-estatica-e-generics/conteudo/typeUnions.ts
@@ -0,0 +1,11 @@
+function retornaCPF(cpf: number | string) {
+  console.log("Seu CPF é: "+ cpf);
+}
+
+type StatesOfMatter = "liquid" | "solid" | "gaseous";
+
+type IndentifyingDocument = number | string;
+
+type OperationalSystems = "Linux" | "Windows" | "Mac";
+
+type Vowels = "a" | "e" | "i" | "o" | "u";
