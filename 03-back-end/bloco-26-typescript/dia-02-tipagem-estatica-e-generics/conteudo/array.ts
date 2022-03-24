38c1f968911b80d04107529b15166ecd4a4c016a
Erickson <ericksonsiqueirasilva@gmail.com>
2022-03-24 20:34:33 -0300

    feat: creating especific array

---
+++ 03-back-end/bloco-26-typescript/dia-02-tipagem-estatica-e-generics/conteudo/array.ts
@@ -0,0 +1,5 @@
+let names: string[] = ["Mary Joe", "Alan Joe"];
+
+let fullName: [string, string] = ["Erickson", "Anna"];
+let person: [string, number] = ["Erickson", 24];
+let car: [string, string, number] = ["Ford", "F400", 10];
