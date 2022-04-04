4b42bcdbf322658c4716023cb4096a78f0f9dfa9
Erickson <ericksonsiqueirasilva@gmail.com>
2022-04-04 16:09:49 -0300

    feat: encapsula classe person

--- 03-back-end/bloco-27-poo-e-solid/dia-01-introducao-a-orientacao-a-objetos/conteudo/Person.ts
+++ 03-back-end/bloco-27-poo-e-solid/dia-01-introducao-a-orientacao-a-objetos/conteudo/Person.ts
@@ -1,34 +1,43 @@
 class Person {
-  private name: string;
-  private height: string;
-  private weight: number;
+  name: string;
+  private _age: number;
+  readonly height: string;
+  private _weight: number;
 
-  constructor(n:string, h: string, w: number) {
+  constructor(n:string, a: number, h: string, w: number) {
     this.name = n;
+    this._age = a; 
     this.height = h;
-    this.weight = w;
+    this._weight = w;
   }
 
-  getName() {
-    return this.name;
+  getAge(): number {
+    return this._age;
   }
-  getHeight() {
-    return this.height;
+  getWeight(): number {
+    return this._weight;
   }
-  getWeight() {
-    return this.weight;
+
+  setAge(newAge: number): void {
+    if(newAge > 0 && newAge < 200) {
+      this._age = newAge;
+    }
   }
 
   sleep():void {
     console.log(`${this.name} zZZzzZZzzzZZz`);
   }
+
+  birthday() {
+    this._age += 1;
+  }
 }
 
-const p1 = new Person('Erickson Siqueira', '1,73', 73);
-const p2 = new Person('Anna Paula', '1,53', 49);
+const p1 = new Person('Erickson Siqueira', 24,'1,73', 73);
+const p2 = new Person('Anna Paula', 21,'1,53', 49);
 
-console.log(p1.getHeight());
-console.log(p2.getHeight());
+console.log(p1.getAge());
+console.log(p2.getWeight());
 
 p1.sleep();
 p2.sleep();
